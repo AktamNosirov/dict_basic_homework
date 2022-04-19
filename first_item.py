3b2e74eec11f87e79a05249577fe50ff82c4d469
Aktam <94_aktam@mail.ru>
2022-04-19 20:43:41 +0500

    4

--- first_item.py
+++ first_item.py
@@ -3,4 +3,7 @@ def first_item():
     Given a dictionary, Return first item value.
     """
     data = {'a': 1, 'b': 2}
-    return
+    
+        
+    return list(data.values())[0]
+print(first_item())
